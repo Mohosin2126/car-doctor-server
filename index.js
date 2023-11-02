599cda592e8ba89e308f340f28562e9287bb1b4b
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-02 20:08:29 +0800

    only needed data loaded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors =require('cors')
 
 const app=express()
 const port=process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 //middleware 
 app.use(cors())
@@ -36,6 +36,21 @@ app.get('/services',async(req,res)=>{
     const result=await cursor.toArray()
     res.send(result)
 })
+
+app.get('/services/:id',async(req,res)=>{
+    const id=req.params.id
+    const query={ _id: new ObjectId(id)}
+    
+    const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+    };
+
+    const result =await serviceCollection.findOne(query , options)
+    res.send(result)
+})
+
+
 
 
     await client.db("admin").command({ ping: 1 });
