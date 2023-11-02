056b798a76591388b851b7051c632e039b622ba5
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-02 19:15:21 +0800

    added mongodb thn added username and password and add .env file and gitignore file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const cors =require('cors')
+
 const app=express()
 const port=process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 //middleware 
 app.use(cors())
 app.use(express.json())
@@ -10,7 +12,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.crat2tn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crat2tn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
