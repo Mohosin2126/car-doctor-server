eada0bb62db473bc51c5948e10e18143d363db1a
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-03 15:25:42 +0800

    data received from the booking services

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
     // Send a ping to confirm a successful connection
 
 const serviceCollection =client.db('carDoctor').collection("services")
+const bookingCollection=client.db("carDoctor").collection("bookings")
 
 app.get('/services',async(req,res)=>{
     const cursor=serviceCollection.find()
@@ -43,13 +44,17 @@ app.get('/services/:id',async(req,res)=>{
     
     const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+        projection: {_id:1, title: 1, price: 1, service_id: 1, img: 1 },
     };
 
     const result =await serviceCollection.findOne(query , options)
     res.send(result)
 })
 
+app.post("/bookings",async(req,res)=>{
+  const booking=req.body
+  console.log(booking)
+})
 
 
 
