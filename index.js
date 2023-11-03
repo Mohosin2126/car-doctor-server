71587ab4e42557a94be0bd9cf9fc9de7c539e39f
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-04 01:42:00 +0800

    send token and get token

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express=require('express')
 const cors =require('cors')
 const jwt=require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const app=express()
 const port=process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 //middleware 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
+}))
 app.use(express.json())
-
+app.use(cookieParser());
 
 
 
@@ -37,7 +41,13 @@ app.post("/jwt",async(req,res)=>{
   const user=req.body 
   console.log(user)
   const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-  res.send(token)
+  res
+  .cookie('token', token, {
+      httpOnly: true,
+      secure: false,
+      samesite:'none'
+  })
+  .send({ success: true })
 })
 
 
