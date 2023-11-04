8b498087a5b61caf0fe7375507f59bb8ba7f7841
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-04 23:38:48 +0800

    done for now

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json())
 app.use(cookieParser());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crat2tn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,7 +38,6 @@ const bookingCollection=client.db("carDoctor").collection("bookings")
 // auth related api
 app.post("/jwt",async(req,res)=>{
   const user=req.body 
-  console.log(user)
   const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
   res
   .cookie('token', token, {
